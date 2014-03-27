ab679759693c821e533081349074a416fbf6a86c
Emil <emil.n.larsson@gmail.com>
2014-03-27 20:42:48 +0100

    Pulled out the cities to an external file

--- multiproc-temp.py
+++ multiproc-temp.py
@@ -2,59 +2,21 @@ import requests
 from multiprocessing.dummy import Pool as ThreadPool
 
 
+def read_cities(file='./cities.txt'):
+    with open(file, 'r') as f:
+        cities = f.read().split('\n')
+    return cities
+
+
+def get(param):
+    """Reduce number of parameters to the get method"""
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
+    return requests.get(URL, params=param)
+
+
 def main():
     """Main method"""
-
-    cities = [
-        'Chicago',
-        'Houston',
-        'Philadelphia',
-        'Phoenix',
-        'San Antonio',
-        'San Diego',
-        'Dallas',
-        'San Jose',
-        'Austin',
-        'Jacksonville',
-        'Indianapolis',
-        'San Francisco',
-        'Columbus',
-        'Fort Worth',
-        'Charlotte',
-        'Detroit',
-        'El Paso',
-        'Memphis',
-        'Boston',
-        'Seattle',
-        'Denver',
-        'Washington',
-        'Nashville-Davidson',
-        'Baltimore',
-        'Louisville-Jefferson County',
-        'Portland',
-        'Oklahoma City',
-        'Milwaukee',
-        'Las Vegas',
-        'Albuquerque',
-        'Tucson',
-        'Fresno',
-        'Sacramento',
-        'Long Beach',
-        'Kansas City',
-        'Mesa',
-        'Virginia Beach',
-        'Atlanta',
-        'Colorado Springs',
-        'Raleigh',
-        'Omaha',
-        'Miami',
-        'Oakland',
-        'Tulsa',
-        'Minneapolis',
-        'Cleveland',
-        'Wichita'
-    ]
-
+    cities = read_cities()
     params = [{'q': city, 'units': 'metric'} for city in cities]
 
     # Make the pool of workers
@@ -63,15 +25,12 @@ def main():
     results = pool.map(get, params)
 
     for r in results:
-        json = r.json()
-        print 'City: {city}, Temp: {temp} degrees'.format(
-            city=json['name'], temp=json['main']['temp'])
-
-
-def get(param):
-    """Reduce number of parameters to the get method"""
-    URL = 'http://api.openweathermap.org/data/2.5/weather'
-    return requests.get(URL, params=param)
+        try:
+            json = r.json()
+            print 'City: {city}, Temp: {temp} degrees'.format(
+                city=json['name'], temp=json['main']['temp'])
+        except:
+            pass
 
 
 if __name__ == '__main__':
